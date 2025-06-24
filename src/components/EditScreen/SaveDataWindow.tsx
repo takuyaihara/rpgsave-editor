fb55fc8d154f6ee7c48f005f59a93b90fb90f6ae
takuyaihara <info@takuya-ihara.jp>
2025-06-24 23:01:02 +0900

    feat: SaveDataWindowに自作JSONエディタを導入＆型定義SaveDataを導出 (#10)
    * feat: SaveDataWindowに自作JSONエディタを導入＆型定義SaveDataを導出
    * refactor: saveDataの型をobjectに変更し、Appの型依存を排除

--- src/components/EditScreen/SaveDataWindow.tsx
+++ src/components/EditScreen/SaveDataWindow.tsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 interface SaveDataEditorProps {
   saveData: object;
 }
 
 export const SaveDataEditor: React.FC<SaveDataEditorProps> = ({ saveData }) => {
+  const [jsonText, setJsonText] = useState<string>("");
+
+  useEffect(() => {
+    setJsonText(JSON.stringify(saveData, null, 2));
+  }, [saveData]);
+
   return (
     <div className="save-data-window">
-      <pre>{JSON.stringify(saveData, null, 2)}</pre>
+      <textarea
+        value={jsonText}
+        onChange={e => setJsonText(e.target.value)}
+        className="json-editor"
+        spellCheck={false}
+      />
     </div>
   );
 };
