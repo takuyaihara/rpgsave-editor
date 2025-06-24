0974de00b24ffa9b35209566fb0ab7c71a7cf7e0
takuyaihara <info@takuya-ihara.jp>
2025-06-24 18:44:26 +0900

    feat: 編集画面のレイアウトとスクロール対応を実装

--- src/components/EditScreen/SaveDataWindow.tsx
+++ src/components/EditScreen/SaveDataWindow.tsx
@@ -7,7 +7,6 @@ interface SaveDataEditorProps {
 export const SaveDataEditor: React.FC<SaveDataEditorProps> = ({ saveData }) => {
   return (
     <div className="save-data-window">
-      <div className="save-data-window__label">セーブデータ</div>
       <pre>{JSON.stringify(saveData, null, 2)}</pre>
     </div>
   );
